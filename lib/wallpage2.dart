aac12c90e2463807b9e4f414b9ec5a70b8121041
Jagu4491 <jagrutisheladiya4491@gmail.com>
2023-03-16 21:57:31 +0530

    wallpaper App add to git

---
+++ lib/wallpage2.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:wallpaper_app/wallpage3.dart';
+
+class wallpage2 extends StatelessWidget {
+  var name;
+  var imgs;
+
+
+  wallpage2(this.name,this.imgs);
+
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(name,style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 25,
+          color: Colors.white
+        ),),
+        backgroundColor: Colors.pinkAccent.shade100,
+      ),
+      body:
+        Column(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 15,
+                      crossAxisSpacing: 15,
+                      mainAxisExtent: 300),
+                  itemBuilder: (context, index) {
+                    return InkWell(
+                      onTap: (){
+                        if(index==0){
+                        Navigator.push(context,
+                          MaterialPageRoute(builder: (context)=>wallpage3())
+                        );
+                        }
+                      },
+                      child: Container(
+                        height: 100,
+                        width: 100,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          image: DecorationImage(
+                            image: AssetImage(imgs[index].toString()),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            //Text(arrvalue[index]['name'].toString(),style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold,color: Colors.black),)
+                          ],
+                        ),
+                      ),
+                    );
+
+                  },
+                  itemCount: 8,
+                ),
+
+              ),
+            ),
+
+          ],
+        ),
+
+
+
+    );
+  }
+}
---
+++ lib/wallpage3.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class wallpage3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green.shade400,
+        title: Text(
+          'Wallpage3',
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            image: DecorationImage(
+                image: AssetImage('assets/images/a1.jpg'), fit: BoxFit.fill)),
+        child: Stack(
+          children: [
+            Positioned(
+              left: 60,
+              bottom: 70,
+              width: 70,
+              height: 70,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.grey.shade400,
+                    borderRadius: BorderRadius.circular(16),
+                    image: DecorationImage(
+                      image: AssetImage('assets/images/info3.png'),
+                      fit: BoxFit.fill,
+                    )),
+              ),
+            ),
+            Positioned(
+              left: 170,
+              bottom: 70,
+              width: 70,
+              height: 70,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.grey.shade400,
+                    borderRadius: BorderRadius.circular(16),
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/save.png'),
+                        fit: BoxFit.fill)),
+              ),
+            ),
+            Positioned(
+              right: 70,
+              bottom: 70,
+              width: 70,
+              height: 70,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.grey.shade400,
+                    borderRadius: BorderRadius.circular(16),
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/apply.png'))),
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding:
+                      const EdgeInsets.only(bottom: 30, right: 10, left: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        'Info',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        'Save',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        'Apply',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
